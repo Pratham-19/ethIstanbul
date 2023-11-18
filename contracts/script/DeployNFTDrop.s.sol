601589968e8f7238c03d35ebe5a6ecc12929a9a1
megabyte0x <yashygkf@gmail.com>
2023-11-18 21:04:01 +0300

    fix: reframe the contracts to make it compatible w/ both testnet & mainnet

--- contracts/script/DeployNFTDrop.s.sol
+++ contracts/script/DeployNFTDrop.s.sol
@@ -12,13 +12,23 @@ contract DeployNFTDrop is Script {
 
     function run() external returns (address) {
         helperConfig = new HelperConfig();
-        (address _airnodeRrp,,, address _automate, address _taskCreator) = helperConfig.activeNetworkConfig();
+        (
+            address _airnodeRrp,
+            address _airNodeAddress,
+            bytes32 _endPointIdUint256,
+            ,
+            ,
+            address _automate,
+            address _taskCreator
+        ) = helperConfig.activeNetworkConfig();
         string memory _nftDropName = helperConfig.nftDropName();
         string memory _nftDropSymbol = helperConfig.nftDropSymbol();
         uint256 _interval = helperConfig.nftDropInterval();
 
         vm.startBroadcast();
-        nftDrop = new NFTDrop(_nftDropName, _nftDropSymbol, _interval, _airnodeRrp, _automate, _taskCreator);
+        nftDrop = new NFTDrop(_nftDropName, _nftDropSymbol, _interval, _airnodeRrp,
+        _airNodeAddress,
+        _endPointIdUint256, _automate, _taskCreator);
         console.log("NFTDrop deployed at: %s", address(nftDrop));
         vm.stopBroadcast();
 
